63651e0a6370fde4a605f69bda2cd9d3a58ebe8c
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-02-12 02:19:50 +0700

    refactor(src): remove unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
-import logo from '@/logo.svg';
-import '@/App.css';
 import Button from '@/components/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button>Hello world !</Button>
-      </header>
-    </div>
+    <Button>Hello world !</Button>
   );
 }
 
