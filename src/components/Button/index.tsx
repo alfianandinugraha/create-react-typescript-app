f0fcfae17f89e607d280ba223f1876f4c2e6432b
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-04-29 11:22:26 +0700

    refactor(button): remove messageConsole and useEffect

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,12 @@
-import React, { ReactElement, useEffect } from 'react'
-import messageConsole from '@/utils/logging'
+import React, { ReactElement } from 'react'
 import useCounter from '@/hooks/useCounter'
 import { ButtonProps } from 'ButtonTypes'
 
 const Button = ({ variant, children }: ButtonProps): ReactElement => {
   const [count, setCount] = useCounter(5)
   const buttonClickHandler = () => {
-    messageConsole()
     setCount('+', 10)
   }
-
-  useEffect(() => console.log(count), [count])
 
   return (
     <button
