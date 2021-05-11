4ca287ec8c2a3e0828cce5765f597eb481667b91
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-05-12 04:53:36 +0700

    feat(src): init style.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from 'react'
 import Button from '@/components/Button'
+import '@/style.css'
 
 const App = (): ReactElement => <Button variant="hi">Click me</Button>
 
