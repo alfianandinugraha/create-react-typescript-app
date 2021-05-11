4284f04d26dfd298b0c2a6b8ca57ed3375cccfaa
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-05-12 05:31:08 +0700

    feat(app): add new homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,86 @@
 import React, { ReactElement } from 'react'
-import Button from '@/components/Button'
 import '@/style.css'
 
-const App = (): ReactElement => <Button variant="hi">Click me</Button>
+const App = (): ReactElement => (
+  <main className="container">
+    <section className="wrapper">
+      <img src="/logo192.png" alt="react icon" className="react-logo" />
+      <h1 className="heading">
+        Create React{' '}
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.typescriptlang.org/"
+        >
+          Typescript
+        </a>{' '}
+        App
+      </h1>
+      <section className="ecosystem">
+        <h2>Ecosystem</h2>
+        <ol>
+          <li>
+            <a
+              href="https://reactrouter.com/web/guides/quick-start"
+              target="_blank"
+              rel="noreferrer"
+            >
+              react-router
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/pmndrs/jotai"
+              target="_blank"
+              rel="noreferrer"
+            >
+              jotai
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://reactcommunity.org/react-transition-group/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              react-transition-group
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://testing-library.com/docs/react-testing-library/intro/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              testing-library
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://chrome.google.com/webstore/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi"
+              target="_blank"
+              rel="noreferrer"
+            >
+              React Developer Tools
+            </a>
+          </li>
+        </ol>
+      </section>
+      <section className="contribute">
+        <h2>Contribute</h2>
+        <p>
+          Please visit{' '}
+          <a
+            href="https://github.com/alfianandinugraha/create-react-typescript-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Create React Typescript App
+          </a>
+        </p>
+      </section>
+    </section>
+  </main>
+)
 
 export default App
