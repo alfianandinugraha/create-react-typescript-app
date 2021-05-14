dd49407fde330d5c87385f2009458c2990272586
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-05-14 17:00:32 +0700

    refactor(public-path): use value from .env

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,11 @@
+require('dotenv')
 const { aliasJest, configPaths, alias } = require('react-app-rewire-alias')
 
 const aliasMap = configPaths('./tsconfig.base.json')
 
 module.exports = function override(config, env) {
   const newConfig = alias(aliasMap)(config, env)
-  newConfig.output.publicPath = './'
+  newConfig.output.publicPath = process.env.PUBLIC_URL
   return newConfig
 }
 module.exports.jest = aliasJest(aliasMap)
