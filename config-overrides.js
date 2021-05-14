a8dbebb85649209c0b49dfeb51c383b7ca4f680f
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-05-14 16:55:53 +0700

    fix(public-path): set publicPath webpack to './'

--- config-overrides.js
+++ config-overrides.js
@@ -2,5 +2,9 @@ const { aliasJest, configPaths, alias } = require('react-app-rewire-alias')
 
 const aliasMap = configPaths('./tsconfig.base.json')
 
-module.exports = alias(aliasMap)
+module.exports = function override(config, env) {
+  const newConfig = alias(aliasMap)(config, env)
+  newConfig.output.publicPath = './'
+  return newConfig
+}
 module.exports.jest = aliasJest(aliasMap)
